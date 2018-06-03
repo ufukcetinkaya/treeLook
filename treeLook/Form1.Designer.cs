7566d7fe36e87abf0b8e685ea512dae2f2965b95
ufukcetinkaya <ufukcetinkayafb@gmail.com>
2018-06-04 00:08:18 +0300

    Add project files.

---
+++ treeLook/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace treeLook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logText = new System.Windows.Forms.RichTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btDonus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // logText
+            // 
+            this.logText.Location = new System.Drawing.Point(12, 12);
+            this.logText.Name = "logText";
+            this.logText.Size = new System.Drawing.Size(776, 106);
+            this.logText.TabIndex = 0;
+            this.logText.Text = "";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 153);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(775, 294);
+            this.treeView1.TabIndex = 1;
+            // 
+            // btDonus
+            // 
+            this.btDonus.Location = new System.Drawing.Point(363, 124);
+            this.btDonus.Name = "btDonus";
+            this.btDonus.Size = new System.Drawing.Size(75, 23);
+            this.btDonus.TabIndex = 2;
+            this.btDonus.Text = "Donustur";
+            this.btDonus.UseVisualStyleBackColor = true;
+            this.btDonus.Click += new System.EventHandler(this.BtDonus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDonus);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.logText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox logText;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btDonus;
+    }
+}
+
---
+++ treeLook/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace treeLook
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private static List<string> _tempDagitik = new List<string>();
+
+        private void BtDonus_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            _tempDagitik = FindDelimiter(logText.Text, false, true);
+            treeView1.Nodes.Add("Logs");
+            for (var i = 0; i < _tempDagitik.Count; i++)
+            {
+                var indie = _tempDagitik[i];
+                var start = indie.IndexOf("{", 0, StringComparison.Ordinal);
+                var substring1 = indie.Substring(start, indie.Length - start);
+                var substring2 = indie.Substring(0, start);
+                treeView1.Nodes[0].Nodes.Add(substring2);
+                treeView1.Nodes[0].Nodes[i].Nodes.Add(substring1);
+            }
+            treeView1.ExpandAll();
+            treeView1.Nodes[0].EnsureVisible();
+        }
+
+        private static List<string> FindDelimiter(string strSource, bool skipFirst, bool indexCalc)
+        {
+            var firstDelimiter = 0;
+            var firstIndex = 0;
+            var secondDelimiter = 0;
+            var secondIndex = 0;
+            var dagitik = new List<string>();
+
+            if (!strSource.Contains("{") || !strSource.Contains("}")) return dagitik;
+
+            int counter;
+
+            for (counter = 0; counter < strSource.Length; counter++)
+            {
+                switch (strSource[counter])
+                {
+                    case '{':
+                        if (!skipFirst)
+                        {
+                            firstDelimiter++;
+                        }
+
+                        if (!indexCalc && firstDelimiter == 1)
+                        {
+                            firstIndex = counter + 1;
+                            indexCalc = true;
+                        }
+
+                        skipFirst = false;
+                        break;
+                    case '}':
+                        secondDelimiter++;
+                        secondIndex = counter - firstIndex;
+                        break;
+                }
+
+                if (firstDelimiter == 0 || firstDelimiter-secondDelimiter != 0) continue;
+                dagitik.Add(strSource.Substring(firstIndex, secondIndex + 1));
+                firstIndex = counter + 1;
+                firstDelimiter = 0;
+                secondDelimiter = 0;
+            }
+
+            return dagitik;
+        }
+    }
+}
